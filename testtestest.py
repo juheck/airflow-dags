db24583f9591586018012b7edebeeece60782fe5
Julien Heck <heck.julien@gmail.com>
2020-02-04 15:36:29 -0800

    Another test

---
+++ testtestest.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import airflow
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return 'Test world!'
+
+dag = DAG('testtesttest', description='Simple tutorial DAG', 
+    start_date=airflow.utils.dates.days_ago(2))
+#,
+#          schedule_interval='0 12 * * *',
+#          start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=2, dag=dag)
+
+hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+dummy_operator >> hello_operator
